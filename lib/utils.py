b781c1273333144dc71855aac57983fb83a0bb48
LMissher <1584805140@qq.com>
2020-01-08 22:29:58 +0800

    change data source

--- lib/utils.py
+++ lib/utils.py
@@ -38,50 +38,58 @@ def get_adjacency_matrix(distance_df_filename, num_of_vertices, id_filename=None
     A: np.ndarray, adjacency matrix
 
     '''
-    if 'npy' in distance_df_filename:
+    mat = pd.read_csv(distance_df_filename, header=None).values
 
-        adj_mx = np.load(distance_df_filename)
+    A = np.zeros((int(num_of_vertices), int(num_of_vertices)),
+                 dtype=np.float32)
 
-        return adj_mx, None
+    A[:, :] = mat[:,:]
 
-    else:
+    return A
+    # if 'npy' in distance_df_filename:
 
-        import csv
+    #     adj_mx = np.load(distance_df_filename)
 
-        A = np.zeros((int(num_of_vertices), int(num_of_vertices)),
-                     dtype=np.float32)
+    #     return adj_mx, None
 
-        distaneA = np.zeros((int(num_of_vertices), int(num_of_vertices)),
-                            dtype=np.float32)
+    # else:
 
-        if id_filename:
+    #     import csv
 
-            with open(id_filename, 'r') as f:
-                id_dict = {int(i): idx for idx, i in enumerate(f.read().strip().split('\n'))}  # 把节点id（idx）映射成从0开始的索引
+    #     A = np.zeros((int(num_of_vertices), int(num_of_vertices)),
+    #                  dtype=np.float32)
 
-            with open(distance_df_filename, 'r') as f:
-                f.readline()
-                reader = csv.reader(f)
-                for row in reader:
-                    if len(row) != 3:
-                        continue
-                    i, j, distance = int(row[0]), int(row[1]), float(row[2])
-                    A[id_dict[i], id_dict[j]] = 1
-                    distaneA[id_dict[i], id_dict[j]] = distance
-            return A, distaneA
+    #     distaneA = np.zeros((int(num_of_vertices), int(num_of_vertices)),
+    #                         dtype=np.float32)
 
-        else:
+    #     if id_filename:
 
-            with open(distance_df_filename, 'r') as f:
-                f.readline()
-                reader = csv.reader(f)
-                for row in reader:
-                    if len(row) != 3:
-                        continue
-                    i, j, distance = int(row[0]), int(row[1]), float(row[2])
-                    A[i, j] = 1
-                    distaneA[i, j] = distance
-            return A, distaneA
+    #         with open(id_filename, 'r') as f:
+    #             id_dict = {int(i): idx for idx, i in enumerate(f.read().strip().split('\n'))}  # 把节点id（idx）映射成从0开始的索引
+
+    #         with open(distance_df_filename, 'r') as f:
+    #             f.readline()
+    #             reader = csv.reader(f)
+    #             for row in reader:
+    #                 if len(row) != 3:
+    #                     continue
+    #                 i, j, distance = int(row[0]), int(row[1]), float(row[2])
+    #                 A[id_dict[i], id_dict[j]] = 1
+    #                 distaneA[id_dict[i], id_dict[j]] = distance
+    #         return A, distaneA
+
+    #     else:
+
+    #         with open(distance_df_filename, 'r') as f:
+    #             f.readline()
+    #             reader = csv.reader(f)
+    #             for row in reader:
+    #                 if len(row) != 3:
+    #                     continue
+    #                 i, j, distance = int(row[0]), int(row[1]), float(row[2])
+    #                 A[i, j] = 1
+    #                 distaneA[i, j] = distance
+    #         return A, distaneA
 
 
 def scaled_Laplacian(W):
